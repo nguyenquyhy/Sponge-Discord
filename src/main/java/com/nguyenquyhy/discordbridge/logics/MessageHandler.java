6cf917077708fd58e9a258d24eb1174e3c9b0476
Vankka2 <vankkaskype@gmail.com>
2016-11-22 20:19:19 +0200

    prefixBlacklist config option

--- src/main/java/com/nguyenquyhy/discordbridge/logics/MessageHandler.java
+++ src/main/java/com/nguyenquyhy/discordbridge/logics/MessageHandler.java
@@ -4,7 +4,6 @@ import com.nguyenquyhy.discordbridge.DiscordBridge;
 import com.nguyenquyhy.discordbridge.models.ChannelConfig;
 import com.nguyenquyhy.discordbridge.models.GlobalConfig;
 import com.nguyenquyhy.discordbridge.utils.TextUtil;
-import de.btobastian.javacord.DiscordAPI;
 import de.btobastian.javacord.entities.message.Message;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
@@ -31,16 +30,17 @@ public class MessageHandler {
         String botid = mod.getBotClient().getYourself().getId();
         String content = TextUtil.formatDiscordEmoji(message.getContent());
         for (ChannelConfig channelConfig : config.channels) {
+        	if(StringUtils.isNotBlank(config.prefixBlacklist)){
+        		if(content.startsWith(config.prefixBlacklist)){
+        			return;
+        	}}        			
             if (StringUtils.isNotBlank(channelConfig.discordId)
                     && channelConfig.minecraft != null
                     && StringUtils.isNotBlank(channelConfig.minecraft.chatTemplate)
                     && message.getChannelReceiver().getId().equals(channelConfig.discordId)
                     && !message.getAuthor().getId().equals(botid)
-                    && !content.contains("!players")
-                    && !content.contains("!ip")
-                    && !content.contains("!website")
                     && !content.contains(TextUtil.SPECIAL_CHAR) /* Not sending back message from this plugin */) {
-                String author = message.getAuthor().getName();
+            	String author = message.getAuthor().getName();
                 Text formattedMessage = TextUtil.formatUrl(String.format(channelConfig.minecraft.chatTemplate.replace("%a", author), content));
                 // This case is used for default account
                 logger.info(formattedMessage.toPlain());
